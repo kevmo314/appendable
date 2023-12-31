3946bce774e8a38fd6be31ca05d88a1559fca07a
Kevin Wang <kevmo314@gmail.com>
2023-12-30 23:44:20 -0500

    feat: add data pointer value reader

--- pkg/btree/node.go
+++ pkg/btree/node.go
@@ -18,53 +18,68 @@ type DataPointer struct {
 	FieldOffset, Length uint32
 }
 
-func (n *Node) encode(w io.Writer) error {
+func (p DataPointer) Value(r io.ReadSeeker) ([]byte, error) {
+	buf := make([]byte, p.Length)
+	if _, err := r.Seek(int64(p.RecordOffset+uint64(p.FieldOffset)), io.SeekStart); err != nil {
+		return nil, err
+	}
+	n, err := r.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	if n != int(p.Length) {
+		return nil, io.ErrUnexpectedEOF
+	}
+	return buf, nil
+}
+
+func (n *Node) WriteTo(w io.Writer) (int64, error) {
 	size := n.Size
 	if n.Leaf {
 		// mark the first bit
 		size |= 1 << 7
 	}
 	if err := encoding.WriteUint8(w, size); err != nil {
-		return err
+		return 0, err
 	}
 	for i := 0; i < int(n.Size); i++ {
 		if err := encoding.WriteUint64(w, n.Keys[i].RecordOffset); err != nil {
-			return err
+			return 0, err
 		}
 		if err := encoding.WriteUint32(w, n.Keys[i].FieldOffset); err != nil {
-			return err
+			return 0, err
 		}
 		if err := encoding.WriteUint32(w, n.Keys[i].Length); err != nil {
-			return err
+			return 0, err
 		}
 	}
 	for i := 0; i < int(n.Size)+1; i++ {
 		if err := encoding.WriteUint64(w, n.Children[i]); err != nil {
-			return err
+			return 0, err
 		}
 	}
-	return nil
+	return 1 + 16*int64(n.Size) + 8*int64(n.Size+1), nil
 }
 
-func (n *Node) decode(r io.Reader) error {
+func (n *Node) ReadFrom(r io.Reader) (int64, error) {
 	size, err := encoding.ReadByte(r)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	n.Size = size & 0x7f
 	n.Leaf = size&(1<<7) != 0
 	for i := 0; i < int(n.Size); i++ {
 		recordOffset, err := encoding.ReadUint64(r)
 		if err != nil {
-			return err
+			return 0, err
 		}
 		fieldOffset, err := encoding.ReadUint32(r)
 		if err != nil {
-			return err
+			return 0, err
 		}
 		length, err := encoding.ReadUint32(r)
 		if err != nil {
-			return err
+			return 0, err
 		}
 		n.Keys[i] = DataPointer{
 			RecordOffset: recordOffset,
@@ -75,9 +90,9 @@ func (n *Node) decode(r io.Reader) error {
 	for i := 0; i < int(n.Size)+1; i++ {
 		child, err := encoding.ReadUint64(r)
 		if err != nil {
-			return err
+			return 0, err
 		}
 		n.Children[i] = child
 	}
-	return nil
+	return 1 + 16*int64(n.Size) + 8*int64(n.Size+1), nil
 }
