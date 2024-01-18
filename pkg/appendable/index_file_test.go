a4f4e83b20aaba824930d33aebbd521d8348ebac
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-01-18 13:12:47 -0500

    `IndexFile` deep equality check (#44)
    * more tests
    
    * introduce deep equality check between index files
    
    * write test, refactor serialize typing

--- pkg/appendable/index_file_test.go
+++ pkg/appendable/index_file_test.go
@@ -17,12 +17,13 @@ We'll use the green_tripdata_2023-01 dataset as our input
 
 Current findings when comparing:
 jsonl <---> csv
-> the field length doesn't align, it seems like JSONL is accounting for ""
+> the field length doesn't align, it seems like JSONL is accounting for "" for strings, while CSV measures raw string values
 */
 func TestIndexFile(t *testing.T) {
 
-	mockJsonl := "{\"id\":\"dentification\"}\n"
-	mockCsv := "id\ndentification\n"
+	mockJsonl := "{\"id\":\"identification\", \"age\":\"cottoneyedjoe\"}\n"
+	mockCsv := "id,age\nidentification,cottoneyedjoe\n"
+
 
 	t.Run("generate index file", func(t *testing.T) {
 		// jsonl
@@ -39,9 +40,7 @@ func TestIndexFile(t *testing.T) {
 		}
 
 		status, res := jif.compareTo(civ)
-
-		fmt.Printf(fmt.Sprintf("%v", len("dentification")))
-
+    
 		if !status {
 			t.Errorf("Not equal\n%v", res)
 		}
@@ -87,7 +86,7 @@ func (i1 *Index) compareIndex(i2 *Index) (bool, string) {
 	for key, records1 := range i1.IndexRecords {
 		records2, ok := i2.IndexRecords[key]
 		if !ok {
-			return false, fmt.Sprintf("key doesn't exist in i2\tkey found in i1: %v", key)
+			return false, fmt.Sprintf("key doesn't exist in i2\tkey found in i1: %v\n%v\t%v", key, i1.IndexRecords, i2.IndexRecords)
 		}
 
 		for i := range records1 {
@@ -124,11 +123,16 @@ func (i1 *IndexFile) compareTo(i2 *IndexFile) (bool, string) {
 	if len(i1.EndByteOffsets) != len(i2.EndByteOffsets) {
 		return false, fmt.Sprintf("endbyteoffsets length not equal\ti1: %v, i2: %v", len(i1.EndByteOffsets), len(i2.EndByteOffsets))
 	}
+  
+	fmt.Printf("endbyteoffsets equal")
 
 	if len(i1.Checksums) != len(i2.Checksums) {
 		return false, fmt.Sprintf("checksums length not equal\ti1: %v, i2: %v", len(i1.Checksums), len(i2.Checksums))
 	}
 
+
+	fmt.Printf("checksums equal")
+  
 	/*
 		for i, _ := range i1.EndByteOffsets {
 			if i1.EndByteOffsets[i] != i2.EndByteOffsets[i] {
@@ -140,5 +144,8 @@ func (i1 *IndexFile) compareTo(i2 *IndexFile) (bool, string) {
 			}
 		}
 	*/
+
+	fmt.Printf("endbyte and checksums deeply equal")
+
 	return true, "great success!"
 }
