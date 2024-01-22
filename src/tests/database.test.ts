51c2315e76035fa7054aff279e6d46f61495214c
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-01-22 13:07:27 -0500

    decode

--- src/tests/database.test.ts
+++ src/tests/database.test.ts
@@ -94,3 +94,43 @@ describe("test query relation", () => {
 		expect(results).toEqual([25]);
 	});
 });
+
+describe("test type bitmask decoding", () => {
+	let mockDataFile: jest.Mocked<DataFile>;
+	let mockIndexFile: jest.Mocked<VersionedIndexFile<any>>;
+	let database: Database<any>;
+	beforeEach(() => {
+		(DataFile.forUrl as jest.Mock).mockReturnValue({
+			get: jest.fn().mockResolvedValue("mocked response"),
+		});
+		mockDataFile = DataFile.forUrl(
+			"http://example.com/data"
+		) as jest.Mocked<DataFile>;
+
+		mockIndexFile = {
+			indexFileHeader: jest.fn(),
+			indexHeaders: jest.fn(),
+			indexRecord: jest.fn(),
+			dataRecord: jest.fn(),
+		} as jest.Mocked<VersionedIndexFile<any>>;
+
+		// instantiate a Database object with given mocked data file and index file
+		database = Database.forDataFileAndIndexFile(mockDataFile, mockIndexFile);
+	});
+
+	it("it should return the correct field rank from fieldtype", async () => {
+		const testCases = [
+			{ fieldType: BigInt(34), expected: new Set<number>([1, 3]) },
+			{ fieldType: BigInt(1), expected: new Set<number>([4]) },
+			{ fieldType: BigInt(2), expected: new Set<number>([3]) },
+			{ fieldType: BigInt(16), expected: new Set<number>([2]) },
+			{ fieldType: BigInt(32), expected: new Set<number>([1]) },
+			{ fieldType: BigInt(33), expected: new Set<number>([1, 4]) },
+		];
+
+		testCases.forEach(({ fieldType, expected }) => {
+			const result = database.decodeType(fieldType);
+			expect(result).toEqual(expected);
+		});
+	});
+});
