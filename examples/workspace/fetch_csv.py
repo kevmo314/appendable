37892f3a022662d65a377b2b77897bf4da709022
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-01-22 16:34:48 -0500

    split-python-script

--- examples/workspace/fetch_csv.py
+++ examples/workspace/fetch_csv.py
@@ -7,7 +7,5 @@ import requests
 
 response = requests.get('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-01.parquet')
 
-pd.read_parquet(io.BytesIO(response.content)).to_json('green_tripdata_2023-01.jsonl', orient='records', lines=True)
-
 df = pd.read_parquet(io.BytesIO(response.content))
 df.to_csv('green_tripdata_2023-01.csv', index=False)
