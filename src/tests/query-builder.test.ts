07c7cc74cabee8269c4a165ca9ad0c1d613a6d2b
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-02-23 13:01:55 -0500

    feat: query validation (#130)
    * feat: query validation
    
    * resolve
    
    * remove async
    
    * fix tests

--- src/tests/query-builder.test.ts
+++ src/tests/query-builder.test.ts
@@ -1,176 +1,169 @@
-// import { Database, Query } from "../db/database";
-// import { QueryBuilder } from "../db/query-builder";
-// import { validateQuery } from "../db/query-validation";
-// import { IndexHeader, IndexMeta } from "../index-file/meta";
+import { Database, Query } from "../db/database";
+import { QueryBuilder } from "../db/query-builder";
+import { validateQuery } from "../db/query-validation";
+import { IndexHeader } from "../index-file/meta";
 
-// describe("test validate queries", () => {
-// 	interface MockSchema {
-// 		[key: string]: {};
-// 		VendorID: {};
-// 		store_and_fwd_flag: {};
-// 		fare_amount: {};
-// 		payment_type: {};
-// 	}
+describe("test validate queries", () => {
+  interface MockSchema {
+    [key: string]: {};
+    VendorID: {};
+    store_and_fwd_flag: {};
+    fare_amount: {};
+    payment_type: {};
+  }
 
-// 	const headers: IndexHeader[] = [
-// 		{
-// 			fieldName: "VendorID",
-// 			fieldTypes: [2],
-// 		},
-// 		{
-// 			fieldName: "store_and_fwd_flag",
-// 			fieldTypes: [3],
-// 		},
-// 		{
-// 			fieldName: "fare_amount",
-// 			fieldTypes: [2],
-// 		},
-// 		{
-// 			fieldName: "payment_type",
-// 			fieldTypes: [3],
-// 		},
-// 	];
+  const headers: IndexHeader[] = [
+    {
+      fieldName: "VendorID",
+      fieldTypes: [2],
+    },
+    {
+      fieldName: "store_and_fwd_flag",
+      fieldTypes: [3],
+    },
+    {
+      fieldName: "fare_amount",
+      fieldTypes: [2],
+    },
+    {
+      fieldName: "payment_type",
+      fieldTypes: [3],
+    },
+  ];
 
-// 	let database: Database<MockSchema>;
+  let database: Database<MockSchema>;
 
-// 	it(`test query builder`, async () => {
-// 		let qb = new QueryBuilder(database);
+  it(`test query builder`, () => {
+    let qb = new QueryBuilder(database);
 
-// 		let qb1 = qb.where("VendorID", "<=", 2);
+    let qb1 = qb.where("VendorID", "<=", 2);
 
-// 		expect(async () => {
-// 			await validateQuery(qb1.toQuery(), headers);
-// 		}).not.toThrow();
-// 	});
+    expect(() => {
+      validateQuery(qb1.toQuery(), headers);
+    }).not.toThrow();
+  });
 
-// 	it(`test basic query chain`, async () => {
-// 		let q = new QueryBuilder(database).where("VendorID", "<=", 2);
-// 		let query = q.toQuery();
+  it(`test basic query chain`, () => {
+    let q = new QueryBuilder(database).where("VendorID", "<=", 2);
+    let query = q.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "VendorID", operation: "<=", value: 2 },
-// 		]);
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "VendorID", operation: "<=", value: 2 },
+    ]);
 
-// 		expect(async () => {
-// 			await validateQuery(query, headers);
-// 		}).not.toThrow();
+    expect(() => {
+      validateQuery(query, headers);
+    }).not.toThrow();
 
-// 		q = q.orderBy("VendorID", "ASC");
-// 		query = q.toQuery();
+    q = q.orderBy("VendorID", "ASC");
+    query = q.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "VendorID", operation: "<=", value: 2 },
-// 		]);
-// 		expect(query.orderBy).not.toBeNull();
-// 		expect(query.orderBy).toEqual([{ key: "VendorID", direction: "ASC" }]);
-// 		expect(async () => {
-// 			await validateQuery(query, headers);
-// 		}).not.toThrow();
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "VendorID", operation: "<=", value: 2 },
+    ]);
+    expect(query.orderBy).not.toBeNull();
+    expect(query.orderBy).toEqual([{ key: "VendorID", direction: "ASC" }]);
+    expect(() => {
+      validateQuery(query, headers);
+    }).not.toThrow();
 
-// 		q = q.select(["VendorID", "store_and_fwd_flag", "fare_amount"]);
-// 		query = q.toQuery();
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "VendorID", operation: "<=", value: 2 },
-// 		]);
-// 		expect(query.orderBy).not.toBeNull();
-// 		expect(query.orderBy).toEqual([{ key: "VendorID", direction: "ASC" }]);
-// 		expect(query.select).not.toBeNull();
-// 		expect(query.select).toEqual([
-// 			"VendorID",
-// 			"store_and_fwd_flag",
-// 			"fare_amount",
-// 		]);
-// 	});
+    q = q.select(["VendorID", "store_and_fwd_flag", "fare_amount"]);
+    query = q.toQuery();
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "VendorID", operation: "<=", value: 2 },
+    ]);
+    expect(query.orderBy).not.toBeNull();
+    expect(query.orderBy).toEqual([{ key: "VendorID", direction: "ASC" }]);
+    expect(query.select).not.toBeNull();
+    expect(query.select).toEqual([
+      "VendorID",
+      "store_and_fwd_flag",
+      "fare_amount",
+    ]);
+  });
 
-// 	it(`test basic derived query chain`, async () => {
-// 		const q0 = new QueryBuilder(database).where("fare_amount", "==", 1);
-// 		let query = q0.toQuery();
+  it(`test basic derived query chain`, () => {
+    const q0 = new QueryBuilder(database).where("fare_amount", "==", 1);
+    let query = q0.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "fare_amount", operation: "==", value: 2 },
-// 		]);
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "fare_amount", operation: "==", value: 1 },
+    ]);
 
-// 		let q1 = q0.orderBy("fare_amount", "DESC");
-// 		query = q1.toQuery();
+    let q1 = q0.orderBy("fare_amount", "DESC");
+    query = q1.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "fare_amount", operation: "==", value: 2 },
-// 		]);
-// 		expect(query.orderBy).not.toBeNull();
-// 		expect(query.orderBy).toEqual([{ key: "fare_amount", direction: "DESC" }]);
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "fare_amount", operation: "==", value: 1 },
+    ]);
+    expect(query.orderBy).not.toBeNull();
+    expect(query.orderBy).toEqual([{ key: "fare_amount", direction: "DESC" }]);
 
-// 		let q2 = q1.select(["fare_amount"]);
-// 		query = q2.toQuery();
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "fare_amount", operation: "==", value: 2 },
-// 		]);
-// 		expect(query.orderBy).not.toBeNull();
-// 		expect(query.orderBy).toEqual([{ key: "fare_amount", direction: "DESC" }]);
-// 		expect(query.select).not.toBeNull();
-// 		expect(query.select).toEqual(["fare_amount"]);
-// 	});
+    let q2 = q1.select(["fare_amount"]);
+    query = q2.toQuery();
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "fare_amount", operation: "==", value: 1 },
+    ]);
+    expect(query.orderBy).not.toBeNull();
+    expect(query.orderBy).toEqual([{ key: "fare_amount", direction: "DESC" }]);
+    expect(query.select).not.toBeNull();
+    expect(query.select).toEqual(["fare_amount"]);
+  });
 
-// 	it(`test multi derived query chain`, async () => {
-// 		const q0 = new QueryBuilder(database).where("fare_amount", "==", 2);
-// 		let query = q0.toQuery();
+  it(`test multi derived query chain`, () => {
+    const q0 = new QueryBuilder(database).where("fare_amount", "==", 2);
+    let query = q0.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "fare_amount", operation: "==", value: 2 },
-// 		]);
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "fare_amount", operation: "==", value: 2 },
+    ]);
 
-// 		let q1 = q0.where("VendorID", "==", 3);
-// 		query = q1.toQuery();
+    let q1 = q0.where("VendorID", "==", 2);
+    query = q1.toQuery();
 
-// 		expect(query.where).not.toBeNull();
-// 		expect(query.where).toEqual([
-// 			{ key: "fare_amount", operation: "==", value: 2 },
-// 			{ key: "VendorID", operation: "==", value: 2 },
-// 		]);
-// 	});
+    expect(query.where).not.toBeNull();
+    expect(query.where).toEqual([
+      { key: "fare_amount", operation: "==", value: 2 },
+      { key: "VendorID", operation: "==", value: 2 },
+    ]);
+  });
 
-// 	it(`test green + red queries`, async () => {
-// 		const q0 = new QueryBuilder(database).where("payment_type", ">", "");
-// 		const failQuery = q0.orderBy("VendorID", "ASC");
-// 		expect(failQuery.toQuery().orderBy).toEqual([
-// 			{ key: "VendorID", direction: "ASC" },
-// 		]);
+  it(`test green + red queries`, () => {
+    const q0 = new QueryBuilder(database).where("payment_type", ">", 3);
+    const failQuery = q0.orderBy("VendorID", "ASC");
+    expect(failQuery.toQuery().orderBy).toEqual([
+      { key: "VendorID", direction: "ASC" },
+    ]);
 
-// 		const passQuery = q0.orderBy("payment_type", "DESC");
-// 		expect(passQuery.toQuery().orderBy).toEqual([
-// 			{ key: "payment_type", direction: "DESC" },
-// 		]);
+    const passQuery = q0.orderBy("payment_type", "DESC");
+    expect(passQuery.toQuery().orderBy).toEqual([
+      { key: "payment_type", direction: "DESC" },
+    ]);
 
-// 		const failQuery2 = passQuery.select(["wef"]);
-// 		const passQuery2 = passQuery.select([
-// 			"VendorID",
-// 			"payment_type",
-// 			"fare_amount",
-// 		]);
+    const failQuery2 = passQuery.select(["wef"]);
+    const passQuery2 = passQuery.select([
+      "VendorID",
+      "payment_type",
+      "fare_amount",
+    ]);
 
-// 		// red queries
-// 		[failQuery, failQuery2].forEach(async (query) => {
-// 			await expect(() =>
-// 				validateQuery(query.toQuery(), headers)
-// 			).rejects.toThrow();
-// 		});
+    // red queries
+    [failQuery, failQuery2].forEach((query) => {
+      expect(() =>
+        validateQuery(query.toQuery(), headers)
+      ).toThrow();
+    });
 
-// 		// green queries
-// 		[passQuery, passQuery2].forEach(async (query) => {
-// 			await expect(() => validateQuery(query.toQuery(), headers)).not.toThrow();
-// 		});
-// 	});
-// });
-
-
-describe('comment out tests for now', () => {
-	it('', () => {
-
-	})
-})
+    // green queries
+    [passQuery, passQuery2].forEach((query) => {
+      expect(() => validateQuery(query.toQuery(), headers)).not.toThrow();
+    });
+  });
+});
--- src/tests/query-validation.test.ts
+++ src/tests/query-validation.test.ts
@@ -1,152 +1,146 @@
-// import { Query } from "../db/database";
-// import { validateQuery } from "../db/query-validation";
-// import { IndexHeader, IndexMeta } from "../index-file/meta";
+import { Query } from "../db/database";
+import { validateQuery } from "../db/query-validation";
+import { IndexHeader } from "../index-file/meta";
 
-// describe("test validate queries", () => {
-// 	interface MockSchema {
-// 		[key: string]: {};
-// 		VendorID: {};
-// 		store_and_fwd_flag: {};
-// 		fare_amount: {};
-// 		payment_type: {};
-// 	}
+describe("test validate queries", () => {
+  interface MockSchema {
+    [key: string]: {};
+    VendorID: {};
+    store_and_fwd_flag: {};
+    fare_amount: {};
+    payment_type: {};
+  }
 
-// 	const headers: IndexHeader[] = [
-// 		{
-// 			fieldName: "VendorID",
-// 			fieldTypes: [2],
-// 		},
-// 		{
-// 			fieldName: "store_and_fwd_flag",
-// 			fieldTypes: [3],
-// 		},
-// 		{
-// 			fieldName: "fare_amount",
-// 			fieldTypes: [2],
-// 		},
-// 		{
-// 			fieldName: "payment_type",
-// 			fieldTypes: [34],
-// 		},
-// 	];
+  const headers: IndexHeader[] = [
+    {
+      fieldName: "VendorID",
+      fieldTypes: [0],
+    },
+    {
+      fieldName: "store_and_fwd_flag",
+      fieldTypes: [6],
+    },
+    {
+      fieldName: "fare_amount",
+      fieldTypes: [3],
+    },
+    {
+      fieldName: "payment_type",
+      fieldTypes: [3, 0],
+    },
+  ];
 
-// 	const validQueries: Query<MockSchema>[] = [
-// 		{
-// 			where: [
-// 				{
-// 					operation: "==",
-// 					key: "VendorID",
-// 					value: 2,
-// 				},
-// 			],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: "<",
-// 					key: "fare_amount",
-// 					value: 100,
-// 				},
-// 			],
-// 			orderBy: [
-// 				{
-// 					key: "fare_amount",
-// 					direction: "ASC",
-// 				},
-// 			],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: ">=",
-// 					key: "payment_type",
-// 					value: 300,
-// 				},
-// 			],
-// 			orderBy: [
-// 				{
-// 					key: "payment_type",
-// 					direction: "DESC",
-// 				},
-// 			],
-// 			select: ["payment_type", "fare_amount"],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: "==",
-// 					key: "store_and_fwd_flag",
-// 					value: "",
-// 				},
-// 			],
-// 			select: ["fare_amount", "payment_type"],
-// 		},
-// 	];
+  const validQueries: Query<MockSchema>[] = [
+    {
+      where: [
+        {
+          operation: "==",
+          key: "VendorID",
+          value: "",
+        },
+      ],
+    },
+    {
+      where: [
+        {
+          operation: "<",
+          key: "fare_amount",
+          value: 10,
+        },
+      ],
+      orderBy: [
+        {
+          key: "fare_amount",
+          direction: "ASC",
+        },
+      ],
+    },
+    {
+      where: [
+        {
+          operation: ">=",
+          key: "payment_type",
+          value: 300,
+        },
+      ],
+      orderBy: [
+        {
+          key: "payment_type",
+          direction: "DESC",
+        },
+      ],
+      select: ["payment_type", "fare_amount"],
+    },
+    {
+      where: [
+        {
+          operation: "==",
+          key: "store_and_fwd_flag",
+          value: false,
+        },
+      ],
+      select: ["fare_amount", "payment_type"],
+    },
+  ];
 
-// 	validQueries.forEach((query) => {
-// 		it("test valid query", async () => {
-// 			// expect(async () => {
-// 			// 	await validateQuery(query, headers);
-// 			// }).not.toThrow();
-// 		});
-// 	});
+  validQueries.forEach((query) => {
+    it("test valid query", () => {
+      expect(() => {
+        validateQuery(query, headers);
+      }).not.toThrow();
+    });
+  });
 
-// 	const notValidQueries: Query<MockSchema>[] = [
-// 		{
-// 			where: [
-// 				{
-// 					operation: "<=",
-// 					key: "vendorid",
-// 					value: 1,
-// 				},
-// 			],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: "==",
-// 					key: "store_and_fwd_flag",
-// 					value: 10,
-// 				},
-// 			],
-// 			orderBy: [
-// 				{
-// 					key: "store_and_flag",
-// 					direction: "ASC",
-// 				},
-// 			],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: "<",
-// 					key: "payment_type",
-// 					value: 100,
-// 				},
-// 			],
-// 			select: ["payment_type", "vendorid", "store_and_fwd_flag"],
-// 		},
-// 		{
-// 			where: [
-// 				{
-// 					operation: "==",
-// 					key: "payment_type",
-// 					value: "",
-// 				},
-// 			],
-// 			select: ["payment_type"],
-// 		},
-// 	];
+  const notValidQueries: Query<MockSchema>[] = [
+    {
+      where: [
+        {
+          operation: "<=",
+          key: "vendorid",
+          value: 1,
+        },
+      ],
+    },
+    {
+      where: [
+        {
+          operation: "==",
+          key: "store_and_fwd_flag",
+          value: 10,
+        },
+      ],
+      orderBy: [
+        {
+          key: "store_an_flag",
+          direction: "ASC",
+        },
+      ],
+    },
+    {
+      where: [
+        {
+          operation: "<",
+          key: "payment_type",
+          value: false,
+        },
+      ],
+      select: ["payment_type", "vendorid", "store_and_fwd_flag"],
+    },
+    {
+      where: [
+        {
+          operation: "==",
+          key: "payment_type",
+          value: "",
+        },
+      ],
+      select: ["paymet_type"],
+    },
+  ];
 
-// 	notValidQueries.forEach((query, index) => {
-// 		// it(`test invalid query ${index}`, async () => {
-// 		// 	await expect(validateQuery(query, headers)).rejects.toThrow();
-// 		// });
-// 	});
-// });
-
-describe('comment out tests for now', () => {
-	it('', () => {
-
-	})
-})
+  notValidQueries.forEach((query, index) => {
+    it(`test invalid query ${index}`, () => {
+      expect(() => validateQuery(query, headers)).toThrow();
+    });
+  });
+});
