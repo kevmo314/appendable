7adb43de7b5f7f9d277855c8c22594739ae2b77d
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-02-23 09:41:27 -0500

    feat: to_string()

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
 import { DataFile } from "./data-file";
-import { Database, FieldType } from "./db/database";
+import { Database, FieldType, fieldTypeToString } from "./db/database";
 import { IndexFile } from "./index-file/index-file";
 import { RangeResolver } from "./resolver";
 
 export async function init(
-	dataUrl: string | RangeResolver,
-	indexUrl: string | RangeResolver
+  dataUrl: string | RangeResolver,
+  indexUrl: string | RangeResolver
 ) {
-	return Database.forDataFileAndIndexFile(
-		typeof dataUrl === "string"
-			? DataFile.forUrl(dataUrl)
-			: DataFile.forResolver(dataUrl),
-		typeof indexUrl === "string"
-			? await IndexFile.forUrl(indexUrl)
-			: await IndexFile.forResolver(indexUrl)
-	);
+  return Database.forDataFileAndIndexFile(
+    typeof dataUrl === "string"
+      ? DataFile.forUrl(dataUrl)
+      : DataFile.forResolver(dataUrl),
+    typeof indexUrl === "string"
+      ? await IndexFile.forUrl(indexUrl)
+      : await IndexFile.forResolver(indexUrl)
+  );
 }
 
 interface GlobalMap {
-	Appendable: {
-		init: Function;
-		FieldType: typeof FieldType;
-	};
+  Appendable: {
+    init: Function;
+    FieldType: typeof FieldType;
+    fieldTypeToString: Function;
+  };
 }
 
 declare global {
-	var Appendable: GlobalMap["Appendable"];
+  var Appendable: GlobalMap["Appendable"];
 }
 
 globalThis.Appendable = {
-	init,
-	FieldType,
+  init,
+  FieldType,
+  fieldTypeToString,
 };
