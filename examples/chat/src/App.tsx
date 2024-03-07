3ec10b65994a8b62b00404bb1ab70145c362cb0e
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-03-07 17:44:53 -0500

    feat: scaffold (#169)

---
+++ examples/chat/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import { init } from "../src/appendable/appendable.min.js";
+
+const App: Component = () => {
+  return (
+    <div class="min-h-screen ">
+      <p>howdy</p>
+    </div>
+  );
+};
+
+export default App;
