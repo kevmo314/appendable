9a4a2b39dddf3f195114c08d6800ec2c4861f663
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-03-07 17:42:14 -0500

    prettier

--- examples/chat/src/App.tsx
+++ examples/chat/src/App.tsx
@@ -1,6 +1,5 @@
-import type { Component } from 'solid-js';
-import { init }  from "../src/appendable/appendable.min.js";
-
+import type { Component } from "solid-js";
+import { init } from "../src/appendable/appendable.min.js";
 
 const App: Component = () => {
   return (
