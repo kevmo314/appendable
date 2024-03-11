29ea8106b7c3e8b80c1cd59a77f9f295654bd003
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2024-03-11 14:31:19 -0400

    fix: remove .index from mock

--- pkg/mocks/main.go
+++ pkg/mocks/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 
+	"github.com/kevmo314/appendable/pkg/appendable"
 	"github.com/kevmo314/appendable/pkg/btree"
 	"github.com/kevmo314/appendable/pkg/buftest"
 	"github.com/kevmo314/appendable/pkg/pagefile"
@@ -134,10 +136,51 @@ func generateFilledMetadata() {
 	b.WriteToDisk("filled_metadata.bin")
 }
 
+func writeByteToFile(data []byte, filename string) error {
+	if err := os.WriteFile(filename, data, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateFileMeta() {
+	fm := appendable.FileMeta{}
+	fm.Format = 1
+	fm.Version = 1
+	fm.ReadOffset = 4096
+
+	b, err := fm.MarshalBinary()
+	if err != nil {
+		log.Fatalf("failed to write file meta to disk")
+	}
+
+	if err := writeByteToFile(b, "filemeta.bin"); err != nil {
+		log.Fatalf("failed to write bytes to disk")
+	}
+}
+
+func generateIndexMeta() {
+	im := appendable.IndexMeta{}
+	im.FieldName = "howdydo"
+	im.FieldType = appendable.FieldTypeBoolean
+	im.Width = appendable.DetermineType(appendable.FieldTypeBoolean)
+
+	b, err := im.MarshalBinary()
+	if err != nil {
+		log.Fatal("failed to write index meta to disk")
+	}
+
+	if err := writeByteToFile(b, "indexmeta.bin"); err != nil {
+		log.Fatalf("failed to write bytes to disk")
+	}
+}
+
 func main() {
 
 	// generateFilledMetadata()
-	generateBasicBtree()
+	// generateBasicBtree()
 	//generateBtreeIterator()
+	// generateFileMeta()
+	generateIndexMeta()
 
 }
