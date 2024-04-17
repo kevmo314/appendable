5d4aa5462a1f0eb8a59c072bf496c3361fe7d19a
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-04-17 10:22:26 -0400

    Revert "chore: remove n offsets code (#243)" (#248)
    This reverts commit e3b87ca0874b118a598cc8bbd982747b82b5dcaa.

--- pkg/metapage/multi.go
+++ pkg/metapage/multi.go
@@ -5,13 +5,14 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"io"
-
 	"github.com/kevmo314/appendable/pkg/btree"
 	"github.com/kevmo314/appendable/pkg/pointer"
+	"io"
 
 	"github.com/kevmo314/appendable/pkg/pagefile"
 )
+
+const N = 16
 
 /**
  * LinkedMetaPage is a linked list of meta pages. Each page contains
@@ -56,10 +57,10 @@ func (m *LinkedMetaPage) BTree(t *btree.BTree) *btree.BTree {
 }
 
 func (m *LinkedMetaPage) Metadata() ([]byte, error) {
-	if _, err := m.rws.Seek(int64(m.offset)+24, io.SeekStart); err != nil {
+	if _, err := m.rws.Seek(int64(m.offset)+(8*N+16), io.SeekStart); err != nil {
 		return nil, err
 	}
-	buf := make([]byte, m.rws.PageSize()-24)
+	buf := make([]byte, m.rws.PageSize()-(8*N+16))
 	if _, err := m.rws.Read(buf); err != nil {
 		return nil, err
 	}
@@ -77,10 +78,10 @@ func (m *LinkedMetaPage) UnmarshalMetadata(bu encoding.BinaryUnmarshaler) error 
 }
 
 func (m *LinkedMetaPage) SetMetadata(data []byte) error {
-	if len(data) > m.rws.PageSize()-24 {
+	if len(data) > m.rws.PageSize()-(8*N+16) {
 		return errors.New("metadata too large")
 	}
-	if _, err := m.rws.Seek(int64(m.offset)+24, io.SeekStart); err != nil {
+	if _, err := m.rws.Seek(int64(m.offset)+(8*N+16), io.SeekStart); err != nil {
 		return err
 	}
 	buf := append(make([]byte, 4), data...)
@@ -97,6 +98,41 @@ func (m *LinkedMetaPage) MarshalMetadata(bm encoding.BinaryMarshaler) error {
 		return err
 	}
 	return m.SetMetadata(buf)
+}
+
+func (m *LinkedMetaPage) NextNOffsets(offsets []uint64) ([]uint64, error) {
+	if _, err := m.rws.Seek(int64(m.offset)+12, io.SeekStart); err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < N; i++ {
+		if err := binary.Read(m.rws, binary.LittleEndian, &offsets[i]); err != nil {
+			return nil, err
+		}
+	}
+
+	return offsets, nil
+}
+
+func (m *LinkedMetaPage) SetNextNOffsets(offsets []uint64) error {
+	if len(offsets) > N {
+		return fmt.Errorf("too many offsets, max number of offsets should be %d", N)
+	}
+
+	if _, err := m.rws.Seek(int64(m.offset)+12, io.SeekStart); err != nil {
+		return err
+	}
+
+	for _, offset := range offsets {
+		if err := binary.Write(m.rws, binary.LittleEndian, offset); err != nil {
+			return err
+		}
+	}
+
+	if err := binary.Write(m.rws, binary.LittleEndian, ^uint64(0)); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *LinkedMetaPage) Next() (*LinkedMetaPage, error) {
