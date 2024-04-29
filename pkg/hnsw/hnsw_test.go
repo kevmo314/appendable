4b30012ec761f515e4dbb4e7015c248328246c6b
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-04-29 13:46:26 -0400

    fix test

--- pkg/hnsw/hnsw_test.go
+++ pkg/hnsw/hnsw_test.go
@@ -5,5 +5,9 @@ import "testing"
 func TestHnsw(t *testing.T) {
 	t.Run("builds graph", func(t *testing.T) {
 		h := NewHNSW(20, 32, 32)
+
+		if h.MaxLayer != -1 {
+			t.Fatalf("expected max layer to default to -1, got %v", h.MaxLayer)
+		}
 	})
 }
