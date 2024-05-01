cbf6ded9e1fb8e509a43da5ef7edced38e7651f9
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-05-01 14:26:16 -0400

    rename conventions (#283)

--- pkg/hnsw/hnsw_test.go
+++ pkg/hnsw/hnsw_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestHnsw(t *testing.T) {
 	t.Run("builds graph", func(t *testing.T) {
-		h := NewHNSW(20, 32, 32)
+		n := NewNode(0, Vector([]float64{0.1, 0.2}))
+		h := NewHNSW(20, 32, 32, n)
 
 		if h.MaxLayer != -1 {
 			t.Fatalf("expected max layer to default to -1, got %v", h.MaxLayer)
