0de05c09f171bc6219c5b506ec4bad51797f58d7
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-06-03 09:35:42 -0400

    feat: hnsw (#319)
    * feat: hnsw
    
    * handle error
    
    * feat: fix assertion
    
    * stateless vector insertg
    
    * friends queue

---
+++ pkg/hnsw/friends.go
@@ -0,0 +1,110 @@
+package hnsw
+
+import (
+	"errors"
+	"math"
+)
+
+type Point []float32
+
+type Friends struct {
+	friends []*BaseQueue
+}
+
+// NewFriends creates a new vector, note the max level is inclusive.
+func NewFriends(maxLevel int) *Friends {
+	friends := make([]*BaseQueue, maxLevel+1)
+
+	for i := 0; i <= maxLevel; i++ {
+		friends[i] = NewBaseQueue(MinComparator{})
+	}
+
+	return &Friends{
+		friends: friends,
+	}
+}
+
+func (v *Friends) Levels() int {
+	return len(v.friends)
+}
+
+func (v *Friends) MaxLevel() int {
+	return len(v.friends) - 1
+}
+
+func (v *Friends) HasLevel(level int) bool {
+	if level < 0 {
+		panic("level must be nonzero positive integer")
+	}
+
+	return level <= v.MaxLevel()
+}
+
+// InsertFriendsAtLevel requires level must be zero-indexed
+func (v *Friends) InsertFriendsAtLevel(level int, vectorId, friendId Id, dist float32) {
+	if !v.HasLevel(level) {
+		panic("failed to insert friends at level, as level is not valId")
+	}
+
+	if friendId == vectorId {
+		panic("cannot insert yourself to friends list")
+	}
+
+	for i := 0; i <= level; i++ {
+		v.friends[i].Insert(friendId, dist)
+	}
+}
+
+func (v *Friends) GetFriendsAtLevel(level int) (*BaseQueue, error) {
+	if !v.HasLevel(level) {
+		return nil, errors.New("failed to get friends at level")
+	}
+
+	return v.friends[level], nil
+}
+
+func EuclidDistance(p0, p1 Point) float32 {
+	var sum float32
+
+	for i := range p0 {
+		delta := p0[i] - p1[i]
+		sum += delta * delta
+	}
+
+	return float32(math.Sqrt(float64(sum)))
+}
+
+// NearlyEqual is sourced from scalar package written by gonum
+// https://pkg.go.dev/gonum.org/v1/gonum/floats/scalar#EqualWithinAbsOrRel
+func NearlyEqual(a, b float32) bool {
+	return EqualWithinAbs(float64(a), float64(b)) || EqualWithinRel(float64(a), float64(b))
+}
+
+// EqualWithinAbs returns true when a and b have an absolute difference
+// not greater than tol.
+func EqualWithinAbs(a, b float64) bool {
+	return a == b || math.Abs(a-b) <= 1e-6
+}
+
+// minNormalFloat64 is the smallest normal number. For 64 bit IEEE-754
+// floats this is 2^{-1022}.
+const minNormalFloat64 = 0x1p-1022
+
+// EqualWithinRel returns true when the difference between a and b
+// is not greater than tol times the greater absolute value of a and b,
+//
+//	abs(a-b) <= tol * max(abs(a), abs(b)).
+func EqualWithinRel(a, b float64) bool {
+	if a == b {
+		return true
+	}
+	delta := math.Abs(a - b)
+	if delta <= minNormalFloat64 {
+		return delta <= 1e-6*minNormalFloat64
+	}
+	// We depend on the division in this relationship to Identify
+	// infinities (we rely on the NaN to fail the test) otherwise
+	// we compare Infs of the same sign and evaluate Infs as equal
+	// independent of sign.
+	return delta/math.Max(math.Abs(a), math.Abs(b)) <= 1e-6
+}
---
+++ pkg/hnsw/friends_test.go
@@ -0,0 +1,135 @@
+package hnsw
+
+import (
+	"math"
+	"testing"
+)
+
+func TestVector_LevelManagement(t *testing.T) {
+
+	/*
+		hex has 6 layers from [0..5]
+		oct has 8 layers from [0..8]
+	*/
+	t.Run("check levels for oct and hex vectors", func(t *testing.T) {
+		hexId := Id(1)
+		hex := []float32{9, 2.0, 30}
+
+		hexFriends := NewFriends(6)
+
+		if hexFriends.MaxLevel() != 6 {
+			t.Fatalf("since 0-indexed, the max level is 5, got: %v", hexFriends.MaxLevel())
+		}
+
+		if hexFriends.Levels() != 7 {
+			t.Fatalf("since 0-indexed, the number of levels is 6, got: %v", hexFriends.Levels())
+		}
+
+		octId := Id(2)
+		oct := []float32{0, 2, 3}
+		octFriends := NewFriends(8)
+
+		if octFriends.MaxLevel() != 8 {
+			t.Fatalf("since 0-indexed, the max level is 7, got: %v", octFriends.MaxLevel())
+		}
+
+		if octFriends.Levels() != 9 {
+			t.Fatalf("since 0-indexed, the number of levels is 8, got: %v", octFriends.Levels())
+		}
+
+		for i := 0; i <= 6; i++ {
+			if !hexFriends.HasLevel(i) {
+				t.Fatalf("since 0-indexed, the level #%v is missing", i)
+			}
+		}
+
+		for i := 7; i <= 8; i++ {
+			if hexFriends.HasLevel(i) {
+				t.Fatalf("since 0-indexed, expected the level #%v to be missing", i)
+			}
+		}
+
+		hexOctDist := EuclidDistance(oct, hex)
+
+		hexFriends.InsertFriendsAtLevel(5, hexId, octId, hexOctDist)
+		octFriends.InsertFriendsAtLevel(5, octId, hexId, hexOctDist)
+
+		for i := 0; i <= 5; i++ {
+			hexFriends, err := hexFriends.GetFriendsAtLevel(i)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			octFriends, err := octFriends.GetFriendsAtLevel(i)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if hexFriends.Len() != 1 || octFriends.Len() != 1 {
+				t.Fatalf("expected hex and oct friends list at level %v to be 1, got: %v || %v", i, hexFriends.Len(), octFriends.Len())
+			}
+
+			top := hexFriends.Peek()
+			if top.id != octId {
+				t.Fatalf("expected %v, got %v", octId, top.id)
+			}
+
+			top = octFriends.Peek()
+			if top.id != hexId {
+				t.Fatalf("expected %v, got %v", hexId, top.id)
+			}
+		}
+	})
+
+}
+
+func TestVector_EuclidDistance(t *testing.T) {
+
+	type vectorPair struct {
+		v0, v1   Point
+		expected float32
+	}
+
+	basic := []vectorPair{
+		{
+			v0:       Point{5, 3, 0},
+			v1:       Point{2, -2, float32(math.Sqrt(2))},
+			expected: 6,
+		},
+		{
+			v0:       Point{1, 0, -5},
+			v1:       Point{-3, 2, -1},
+			expected: 6,
+		},
+		{
+			v0:       Point{1, 3},
+			v1:       Point{5, 2},
+			expected: float32(math.Sqrt(17)),
+		},
+		{
+			v0:       Point{0, 1, 4},
+			v1:       Point{2, 9, 1},
+			expected: float32(math.Sqrt(77)),
+		},
+		{
+			v0:       Point{0},
+			v1:       Point{0},
+			expected: 0,
+		},
+		{
+			v0:       Point{10, 20, 30, 40},
+			v1:       Point{10, 20, 30, 40},
+			expected: 0,
+		},
+	}
+
+	t.Run("correctly computes the distance of two vectors", func(t *testing.T) {
+		for i, pair := range basic {
+			dist := EuclidDistance(pair.v1, pair.v0)
+
+			if !NearlyEqual(dist, pair.expected) {
+				t.Fatalf("iter i: %v, expected %v and %v to be equal", i, dist, pair.expected)
+			}
+		}
+	})
+}
