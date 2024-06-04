a8cabd0e5f6b52dcc3b683c6393a99bb1b084280
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-06-04 11:41:55 -0400

    search layer given a cluster (#324)
    * search layer given a cluster
    
    * add approximate

--- pkg/hnsw/hnsw_test.go
+++ pkg/hnsw/hnsw_test.go
@@ -1,8 +1,11 @@
 package hnsw
 
-import "testing"
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
 
-/*
 var clusterA = []Point{
 	{0.2, 0.5},
 	{0.2, 0.7},
@@ -17,6 +20,7 @@ var clusterA = []Point{
 	{0.01, 0.3},
 }
 
+/*
 var clusterB = []Point{
 	{4.2, 3.5},
 	{4.2, 4.7},
@@ -32,6 +36,56 @@ var clusterB = []Point{
 	{4.01, 4.3},
 }
 */
+
+func SetupClusterAHnsw() (*Hnsw, error) {
+	efc := uint(4)
+
+	entryPoint := Point{0, 0}
+	g := NewHnsw(2, efc, 4, entryPoint)
+
+	for idx, point := range clusterA {
+		pointId := Id(idx + 1)
+		g.points[pointId] = &point
+		g.friends[pointId] = NewFriends(0)
+
+		distEntryToClusterPoint := EuclidDistance(entryPoint, point)
+		g.friends[Id(0)].InsertFriendsAtLevel(0, pointId, distEntryToClusterPoint)
+		g.friends[pointId].InsertFriendsAtLevel(0, Id(0), distEntryToClusterPoint)
+	}
+
+	for idx, pointA := range clusterA {
+		for jdx, pointB := range clusterA {
+			if idx == jdx {
+				continue
+			}
+
+			pointAId := Id(idx + 1)
+			pointBId := Id(jdx + 1)
+
+			distAToB := EuclidDistance(pointA, pointB)
+			g.friends[pointAId].InsertFriendsAtLevel(0, pointBId, distAToB)
+			g.friends[pointBId].InsertFriendsAtLevel(0, pointAId, distAToB)
+		}
+	}
+
+	for kdx := range clusterA {
+		pointId := Id(kdx + 1)
+		friends, err := g.friends[pointId].GetFriendsAtLevel(0)
+		if err != nil {
+			return nil, err
+		}
+
+		for friends.Len() > int(efc) {
+			friends.Pop()
+		}
+
+		if friends.Len() != int(efc) {
+			return nil, errors.New("not all friends length follow the efc parameter")
+		}
+	}
+
+	return g, nil
+}
 
 func TestHnsw_SearchLevel(t *testing.T) {
 	t.Run("search level 0", func(t *testing.T) {
@@ -64,4 +118,89 @@ func TestHnsw_SearchLevel(t *testing.T) {
 			t.Fatalf("expected item id to be %v, got %v", 1, closestItem.id)
 		}
 	})
+
+	t.Run("cluster a searchLayer for existing point", func(t *testing.T) {
+		g, err := SetupClusterAHnsw()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		entryPoint, ok := g.points[Id(0)]
+		if !ok {
+			t.Fatal(ErrNodeNotFound)
+		}
+
+		qPoint := clusterA[3]
+		expectedId := Id(4)
+
+		expectedPoint := g.points[expectedId]
+
+		if !reflect.DeepEqual(qPoint, *expectedPoint) {
+			t.Fatalf("expected point to be %v, got %v", expectedPoint, qPoint)
+		}
+
+		closestNeighbor, err := g.searchLevel(&qPoint, &Item{
+			id:   0,
+			dist: EuclidDistance(*entryPoint, qPoint),
+		}, 1, 0)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if closestNeighbor.IsEmpty() {
+			t.Fatalf("expected # of neighbors to return to be 1, got %v", closestNeighbor)
+		}
+
+		closestItem, err := closestNeighbor.PopItem()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if closestItem.id != expectedId {
+			t.Fatalf("expected the closest item to be the 3rd point in Cluster a, got %v", closestItem.id)
+		}
+	})
+
+	t.Run("cluster a searchLayer for new point", func(t *testing.T) {
+		g, err := SetupClusterAHnsw()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		entryPoint, ok := g.points[Id(0)]
+		if !ok {
+			t.Fatal(ErrNodeNotFound)
+		}
+
+		qPoint := Point{0.3, 0.81}
+		expectedId := Id(3) // point3 is (0.3, 0.8)
+
+		closestNeighbor, err := g.searchLevel(&qPoint, &Item{
+			id:   0,
+			dist: EuclidDistance(*entryPoint, qPoint),
+		}, 1, 0)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if closestNeighbor.IsEmpty() {
+			t.Fatalf("expected # of neighbors to return to be 1, got %v", closestNeighbor)
+		}
+
+		closestItem, err := closestNeighbor.PopItem()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if closestItem.id != expectedId {
+			t.Fatalf("expected the closest item to be the 3rd point in Cluster a, got %v", closestItem.id)
+		}
+	})
+
 }
